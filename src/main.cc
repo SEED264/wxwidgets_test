9cbf3b3ae383652470633908a4e9d0b00e644335
SEED264 <seedx264@gmail.com>
2020-07-09 02:53:23 +0900

    Initial commit

---
+++ src/main.cc
@@ -0,0 +1,8 @@
+#include <iostream>
+#include <wx/wx.h>
+
+int main(int argc, char **argv) {
+    std::cout << "Initial commit\n"
+              << "wxWidget version : " << wxVERSION_FLOAT << std::endl;
+    return 0;
+}
