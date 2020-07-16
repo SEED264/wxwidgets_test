9e28fb501883eed63594874a8fe7c01178c78158
SEED264 <seedx264@gmail.com>
2020-07-16 17:41:14 +0900

    Add menu bar and status bar

--- src/main_frame.h
+++ src/main_frame.h
@@ -17,6 +17,19 @@ public:
 
 inline MainFrame::MainFrame() :
     wxFrame(nullptr, wxID_ANY, "MainFrame", wxDefaultPosition, wxDefaultSize) {
+    // Set menu bar
+    wxMenu *menu_file = new wxMenu;
+    menu_file->Append(wxID_EXIT, "&Quit", "Quit application");
+
+    wxMenuBar *menu_bar = new wxMenuBar;
+    menu_bar->Append(menu_file, "&File");
+
+    SetMenuBar(menu_bar);
+
+    // Set status bar
+    CreateStatusBar();
+    SetStatusText("MainFrame");
+
     // Bind events
     Bind(wxEVT_MENU, &MainFrame::OnClose, this, wxID_EXIT);
 }
