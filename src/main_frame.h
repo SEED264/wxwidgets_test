8081b470c9b50e1092ab1ec55463a5a1610aef4f
SEED264 <seedx264@gmail.com>
2020-07-16 17:38:29 +0900

    Change event binding method

--- src/main_frame.h
+++ src/main_frame.h
@@ -12,17 +12,13 @@ public:
     void OnClose(wxCommandEvent& event) {
         Close(true);
     }
-private:
-    wxDECLARE_EVENT_TABLE();
 };
 
-wxBEGIN_EVENT_TABLE(MainFrame, wxFrame)
-EVT_MENU(wxID_EXIT, MainFrame::OnClose)
-wxEND_EVENT_TABLE()
 
 inline MainFrame::MainFrame() :
-    wxFrame(nullptr, wxID_ANY, "MainFrame", wxPoint(0, 0), wxSize(1280, 720)) {
-    Centre();
+    wxFrame(nullptr, wxID_ANY, "MainFrame", wxDefaultPosition, wxDefaultSize) {
+    // Bind events
+    Bind(wxEVT_MENU, &MainFrame::OnClose, this, wxID_EXIT);
 }
 
 #endif // _WXWIDGETS_TEST_SRC_MAIN_FRAME_H_
